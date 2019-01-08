c85e5b8a0be48170fc5624354c28a31c42b2b6fd
i345010 <efrat.tsadok@sap.com>
2019-01-08 15:14:21 +0200

    transfer random seed to main

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	m "final_project2/manager"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
+	//Create uniq seed for this program - different random numbers every time
+	rand.Seed(time.Now().UnixNano())
+
 	//Create the manager and start the game
 	manager := m.GetInstance()
+
 	//Arguments: Number of players, Probability of loosing messages
 	if err := manager.StartGame(5, 1); err != nil {
 		fmt.Println("Something went wrong!")
