acad27e6613d78dbd6ce5473cce95c4f99548fad
i345010 <efrat.tsadok@sap.com>
2019-05-20 16:41:34 +0300

    Transform the Main class to a GO Server

--- main.go
+++ main.go
@@ -3,11 +3,27 @@ package main
 import (
 	m "final_project2/manager"
 	"fmt"
+	"github.com/gorilla/mux"
 	"math/rand"
+	"net/http"
 	"time"
 )
 
 func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book ###: %s on page %s\n", title, page)
+	})
+
+	http.ListenAndServe(":8000", r)
+	//runScenario()
+}
+
+func runScenario() {
 	//Create uniq seed for this program - different random numbers every time
 	rand.Seed(time.Now().UnixNano())
 
@@ -15,7 +31,7 @@ func main() {
 	manager := m.GetInstance()
 
 	//Arguments: Number of players, Probability of loosing messages
-	if err := manager.StartGame(5, 0.9); err != nil {
+	if err := manager.StartGame(5, 1); err != nil {
 		fmt.Println("Something went wrong!")
 	}
 }
